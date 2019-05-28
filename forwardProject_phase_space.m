597d2e479bbca9f5a4717a29393fb96d7ec58e89
bbncWLG <51014979+bbncWLG@users.noreply.github.com>
2019-05-28 19:32:54 +0800

    Update forwardProject_phase_space.m

--- forwardProject_phase_space.m
+++ forwardProject_phase_space.m
@@ -7,8 +7,7 @@ function projection = forwardProject_phase_space(H,realspace)
 %
 % The Code is created based on the method described in the following paper 
 %   [1]  ZHI LU, JIAMIN WU, HUI QIAO and YOU ZHOU.etc,
-%        Phase-space deconvolution for light field microscopy
-%        Optics Express, 2019. 
+%        Phase-space deconvolution for light field microscopy. 
 %   [2] Robert Prevede, Young-Gyu Yoon, Maximilian Hoffmann, Nikita Pak.etc. 
 %       "Simultaneous whole-animal 3D imaging of neuronal activity using light-field microscopy "   
 %       in Nature Methods VOL.11 NO.7|July 2014.
